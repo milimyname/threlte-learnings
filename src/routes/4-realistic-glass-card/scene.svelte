7194f18d31c02a795261bf0e12b6e12ea78f5da4
mili <kamil.maksudov2019@gmail.com>
2025-08-02 23:38:21 +0200

    chore: rename folders

---
+++ src/routes/4-realistic-glass-card/scene.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[0, 0, 4]} fov={50} />
+
+<T.AmbientLight intensity={0.49} />
+<T.DirectionalLight position={[0.1, -4, 13.9]} />
+
+<T.Mesh rotation={[0, 2, 0]}>
+	<T.BoxGeometry args={[1, 1, 1]} />
+	<T.MeshStandardMaterial color="#0078ff" />
+</T.Mesh>
